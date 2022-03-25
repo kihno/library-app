8e77d6992f35ff2fe487cb8a3be5f140e7bdd4dd
kihno <stistyle13@gmail.com>
2022-03-25 16:13:36 -0700

    initialize html css and js files

---
+++ javascript.js
@@ -0,0 +1,17 @@
+let myLibrary = [];
+
+function Book(title, author, pages) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+}
+
+function addBookToLibrary() {
+
+}
+
+function displayLibrary() {
+const bookshelf = document.getElementById('bookshelf');
+const newDiv = document.createElement('div');
+
+}
