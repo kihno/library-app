c0ba1182f6a55c3613f9875eb38181f6d2e1a85d
kihno <stistyle13@gmail.com>
2022-03-29 16:05:44 -0700

    create function for add book modal popup

--- javascript.js
+++ javascript.js
@@ -1,4 +1,23 @@
-let myLibrary = [];
+let myLibrary = [
+    {
+        title: 'book1',
+        author: 'author one',
+        pages: 123,
+        read: true,
+    },
+    {
+        title: 'book2',
+        author: 'author two',
+        pages: 234,
+        read: false,
+    },
+    {
+        title: 'book3',
+        author: 'author three',
+        pages: 345,
+        read: true,
+    }
+];
 
 function Book(title, author, pages) {
     this.title = title
@@ -11,6 +30,25 @@ const title = document.getElementById('title');
 const author = document.getElementById('author');
 const pages = document.getElementById('pages');
 const read = document.getElementById('read');
+
+const modal = document.getElementById('modal');
+const modalBtn = document.getElementById('addBook');
+const closeModal = document.getElementsByClassName('close')[0];
+
+modalBtn.onclick = function() {
+    modal.style.display = 'grid';
+    title.focus();
+}
+
+closeModal.onclick = function() {
+    modal.style.display = 'none';
+}
+
+// window.onclick = function(e) {
+//     if (e.target !== modal) {
+//         modal.style.display = 'none';
+//     }
+// }
 
 function addBookToLibrary() {
 
@@ -37,11 +75,19 @@ function clearForm() {
     read.checked = false;
 }
 
+const form = document.getElementsByTagName('form')[0];
+
 submit.addEventListener('click', function() {
-    addBookToLibrary();
-    clearForm();
-    const newBook = myLibrary[myLibrary.length - 1];
-    displayLibrary(newBook);
+    
+    /* if (!title.validity.valid) {
+        alert('error');
+    } else { */
+    
+        modal.style.display = 'none';
+        addBookToLibrary();
+        clearForm();
+        const newBook = myLibrary[myLibrary.length - 1];
+        displayLibrary(newBook);
 })
 
 const bookshelf = document.getElementById('bookshelf');
